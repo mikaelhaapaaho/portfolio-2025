42e1c42779deaa368824c149563d1979ce9088d9
Mikael Haapa-aho <mikael.haapa-aho@kuubi.fi>
2025-05-06 23:35:33 +0300

    removed redundancy

--- app.vue
+++ app.vue
@@ -4,16 +4,3 @@
     <NuxtPage />
   </div>
 </template>
-
-<script setup lang="ts">
-import { ref, onMounted } from "vue"
-</script>
-
-<style>
-html,
-body {
-  margin: 0;
-  padding: 0;
-  min-height: 100vh;
-}
-</style>
