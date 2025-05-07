a8f98da857f46297519208e224cf6c87684a1c58
Mikael Haapa-aho <mikael.haapa-aho@kuubi.fi>
2025-05-07 17:15:47 +0300

    added introduction section

---
+++ storyblok/Introduction.vue
@@ -0,0 +1,37 @@
+<template>
+  <section id="introduction" class="container pb-4 mb-5">
+    <div ref="section">
+      <i class="bi bi-quote display-1 text-danger"></i>
+      <div class="border border-primary border-3 p-4">
+        <h2 class="my-4" style="white-space: pre-line">
+          {{ blok.text }}
+        </h2>
+      </div>
+      <div style="transform: rotate(180deg)">
+        <i class="bi bi-quote display-1 text-danger"></i>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script setup>
+import { useAnimations } from "@/composables/useAnimations"
+
+const props = defineProps({ blok: Object })
+const section = ref(null)
+const { scaleIn } = useAnimations()
+
+onMounted(async () => {
+  scaleIn(section.value)
+})
+</script>
+
+<style scoped>
+section {
+  transition: transform 0.3s ease-in-out;
+}
+
+section:hover {
+  transform: scale(1.05);
+}
+</style>
