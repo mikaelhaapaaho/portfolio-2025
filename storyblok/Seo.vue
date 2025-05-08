ede6ddb8ea6e9c841ba485c93fa829f31a75e59c
Mikael Haapa-aho <mikael.haapa-aho@kuubi.fi>
2025-05-08 18:45:02 +0300

    more seo

--- storyblok/Seo.vue
+++ storyblok/Seo.vue
@@ -1,6 +1,9 @@
 <template></template>
 
 <script setup>
+import { useI18n } from "vue-i18n"
+
+const { locale } = useI18n()
 const props = defineProps({ blok: Object })
 
 useHead({
@@ -9,6 +12,18 @@ useHead({
     {
       name: "description",
       content: props.blok.description,
+    },
+    {
+      name: "viewport",
+      content: "width=device-width, initial-scale=1",
+    },
+    {
+      name: "robots",
+      content: "index, follow",
+    },
+    {
+      name: "language",
+      content: locale.value,
     },
     {
       property: "og:title",
@@ -25,6 +40,34 @@ useHead({
     {
       property: "og:image",
       content: props.blok.image.filename,
+    },
+    {
+      property: "og:url",
+      content: window.location.href,
+    },
+    {
+      property: "og:site_name",
+      content: props.blok.title,
+    },
+    {
+      name: "twitter:card",
+      content: "summary_large_image",
+    },
+    {
+      name: "twitter:title",
+      content: props.blok.title,
+    },
+    {
+      name: "twitter:description",
+      content: props.blok.description,
+    },
+    {
+      name: "twitter:image",
+      content: props.blok.image.filename,
+    },
+    {
+      rel: "canonical",
+      href: window.location.href,
     },
   ],
 })
