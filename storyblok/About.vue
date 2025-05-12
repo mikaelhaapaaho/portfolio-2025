e9f9423a00108f7b7ef992ecc485309a97c39c7c
Mikael Haapa-aho <mikael.haapa-aho@kuubi.fi>
2025-05-12 22:27:42 +0300

    changed spline to image

--- storyblok/About.vue
+++ storyblok/About.vue
@@ -3,9 +3,9 @@
     <h3 class="display-1 text-primary overflow-hidden mb-4 lh-base">
       <div><span class="text-danger">/</span>{{ blok.heading }}</div>
     </h3>
-    <div class="row row-cols-1 row-cols-lg-2 text-primary">
-      <div class="col mb-5 mb-lg-0">
-        <div class="bg-light">
+    <div class="row row-cols-1 row-cols-lg-2 text-primary mb-5">
+      <div class="col mb-4 mb-lg-0">
+        <div class="bg-light h-100 d-flex flex-column">
           <div class="row m-0 row-cols-3">
             <div
               class="col border border-bottom-0 border-3 border-primary text-center py-4 px-2 d-flex flex-column justify-content-between"
@@ -28,13 +28,24 @@
               <div>{{ blok.location }}</div>
             </div>
           </div>
-          <div class="border border-3 border-primary p-4">
+          <div class="border border-3 border-primary p-4 flex-grow-1">
             <div v-html="text"></div>
           </div>
         </div>
       </div>
       <div class="col d-flex align-items-center">
-        <canvas class="w-100" style="aspect-ratio: 1/1" ref="canvas" />
+        <div
+          class="border border-3 border-primary px-5 h-100 d-flex align-items-end position-relative bg-light"
+        >
+          <div class="position-absolute top-0 end-0 me-3 mt-2 rotate">
+            <i class="bi bi-flower1 text-danger display-2"></i>
+          </div>
+          <img
+            class="img-fluid px-0 px-md-5 px-lg-0 px-xl-5 pt-4"
+            :src="blok.image.filename"
+            :alt="blok.image.alt"
+          />
+        </div>
       </div>
     </div>
   </section>
@@ -44,25 +55,7 @@
 const props = defineProps({ blok: Object })
 const text = renderRichText(props.blok.text)
 
-import { Application } from "@splinetool/runtime"
-
-const canvas = ref(null)
-
-const state = reactive({
-  spline: {
-    scene: props.blok.splineLink,
-    app: null,
-    isLoaded: false,
-  },
-})
-
-onMounted(async () => {
-  const app = new Application(canvas.value)
-  await app.load(state.spline.scene)
-  state.spline.app = app
-  state.spline.isLoaded = true
-})
-
+// Age and experience calculations
 const age = Math.floor(
   (new Date().getTime() - new Date(1995, 6, 11).getTime()) /
     (1000 * 60 * 60 * 24 * 365.25)
@@ -86,5 +79,18 @@ h3:hover {
 
 .bg-light:hover {
   transform: scale(1.05);
+}
+
+@keyframes rotate360 {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
+
+.rotate {
+  animation: rotate360 10s linear infinite;
 }
 </style>
